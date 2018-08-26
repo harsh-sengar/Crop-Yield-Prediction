f06369da4f09f943ca179708a2f480bf7fc0a31c
Aruvansh1997 <aruvanshn@gmail.com>
2018-08-27 01:01:44 +0530

    static files

--- farming/farming/settings.py
+++ farming/farming/settings.py
@@ -135,3 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   # os.path.join(BASE_DIR, "static"),
+    '/Users/Aruvansh/Desktop/IndiaFarm/farming/frontpage/static/',
+]
+STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
